cb8cada3c5cba375e6cf8f59f3fc5d7698bf29c1
root <root@8ee643f75f83>
2016-04-27 11:48:11 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,10 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "defaultLanguage" : "en",
+  "displayName" : "testhsql",
+  "homePage" : "Main",
+  "name" : "testhsql",
+  "platformType" : "WEB",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
